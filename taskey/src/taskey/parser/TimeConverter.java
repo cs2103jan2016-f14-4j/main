6d34234fff20d4ed6f5095381cddd954f8c6d4d4
XueHui <nattaro13@gmail.com>
2016-02-13 21:56:35 +0800

    Added comments to TimeConverter

--- taskey/src/taskey/parser/TimeConverter.java
+++ taskey/src/taskey/parser/TimeConverter.java
@@ -8,14 +8,35 @@ public class TimeConverter {
 	
 	//store curr time in seconds 
 	private long currTime = System.currentTimeMillis()/1000;
+	//======================================================
 	
-	
-	public long getCurrTime() {
-		return currTime; 
+	/**
+	 * Constructor 
+	 */
+	public TimeConverter() {
+		
 	}
 	
-	public long toEpochTime(String date) {
-		//note that date has to be of the format eg: "01 Jan 1970 01:00:00" 
+	/**
+	 * @return the value of the variable, currTime 
+	 */
+	public long getCurrTime() {
+		return currTime; 
+	}	
+	
+	/**
+	 * Method to update currentTime in this class. 
+	 */
+	public void setCurrTime() {
+		currTime = System.currentTimeMillis()/1000;
+	}
+	
+	/**
+	 * Convert a properly formatted date to its Epoch Format
+	 * @param date: String in the format: dd MMM yyyy HH:mm:ss
+	 * @return date in Epoch Time. 
+	 */
+	public long toEpochTime(String date) { 
 		try {
 			long epochTime = new java.text.SimpleDateFormat("dd MMM yyyy HH:mm:ss").parse(
 					date).getTime() / 1000;
@@ -26,6 +47,11 @@ public class TimeConverter {
 		}	
 	}
 	
+	/**
+	 * Convert an epoch time to human readable time. 
+	 * @param epochTime: a long number that you want to convert to human readable format
+	 * @return String: human readable time 
+	 */
 	public String toHumanTime(long epochTime) {
 		String humanTime = new java.text.SimpleDateFormat("dd MMM yyyy HH:mm:ss").format(
 				new java.util.Date(epochTime*1000));
@@ -33,7 +59,12 @@ public class TimeConverter {
 		return humanTime; 
 	}
 	
-	
+	/**
+	 * Checks if 2 given times in Epoch Format are in the same day
+	 * @param epochTime1
+	 * @param epochTime2
+	 * @return true if they are in the same day 
+	 */
 	public boolean isSameDay(long epochTime1, long epochTime2) {
 		int yearDiff = Math.abs(getYear(epochTime1) - getYear(epochTime2)); 
 		int monthDiff = Math.abs(getMonth(epochTime1) - getMonth(epochTime2));
@@ -49,7 +80,12 @@ public class TimeConverter {
 		return false; 
 	}
 	
-	
+	/**
+	 * Checks if 2 given times in Epoch Format are in the same week
+	 * @param epochTime1
+	 * @param epochTime2
+	 * @return true if they are in the same week 
+	 */
 	public boolean isSameWeek(long epochTime1, long epochTime2) {
 		int yearDiff = Math.abs(getYear(epochTime1) - getYear(epochTime2)); 
 		int monthDiff = Math.abs(getMonth(epochTime1) - getMonth(epochTime2));
@@ -65,7 +101,10 @@ public class TimeConverter {
 		return false; 
 	}
 	
-	
+	/**
+	 * @param epochTime
+	 * @return Year as an integer 
+	 */
 	public int getYear(long epochTime) {
 		String year = new java.text.SimpleDateFormat("yyyy").format(
 				new java.util.Date(epochTime*1000));
@@ -73,7 +112,10 @@ public class TimeConverter {
 		return Integer.parseInt(year); 
 	}
 	
-	
+	/**
+	 * @param epochTime
+	 * @return Month as an integer (0-12) 
+	 */
 	public int getMonth(long epochTime) {
 		String month = new java.text.SimpleDateFormat("MM").format(
 				new java.util.Date(epochTime*1000));
@@ -81,6 +123,10 @@ public class TimeConverter {
 		return Integer.parseInt(month); 
 	}
 	
+	/**
+	 * @param epochTime
+	 * @return day as an integer (0-31) 
+	 */
 	public int getDay(long epochTime) {
 		String day = new java.text.SimpleDateFormat("dd").format(
 				new java.util.Date(epochTime*1000));

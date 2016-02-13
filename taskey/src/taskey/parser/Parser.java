ee176b5f06c560fef136398c04c05eafb5473f22
XueHui <nattaro13@gmail.com>
2016-02-14 01:08:01 +0800

    Added skeleton logic to Parser: only able to add tasks without dates for now

--- taskey/src/taskey/parser/Parser.java
+++ taskey/src/taskey/parser/Parser.java
@@ -1,23 +1,114 @@
 package taskey.parser;
 
+import java.util.ArrayList;
 import taskey.logic.Task;
 import taskey.storage.Storage;
 
 public class Parser {
-	private static Parser instance = null; 
+	
+	public enum specialDays {
+		tomorrow,today,next_week, tonight, this_weekend 
+	}
+	 
+	
+	public Parser() {
+		//TODO 
+	}
+	
+	/**
+	 * Process the user's command and execute it accordingly 
+	 * @param command: string of command keyed in by user
+	 * @return message to be displayed
+	 */
+	public String parseInput(String stringInput) {
+		String message=""; 
+		String command = getCommand(stringInput); 
+		String task = getTaskName(command, stringInput);
+		ArrayList<String> dateRange = getDate(stringInput); 
+		
+		
+		message = executeCommand(command, task);
+		
+		return message;  
+	}
+	
+	//TODO
+	public String executeCommand(String command, String task) {
+		
+		switch(command) {
+			case "add": 
+				//add task to storage!
+				break;
+			case "del":
+				break;
+			case "edit":
+				break;
+			case "view":
+				break;
+			case "sort":
+				break;
+			case "search":
+				break; 
+			
+			default:
+				//return an error message. 		
+		
+		}
+		
+		return ""; 
+	}
+	
+	private String getCommand(String stringInput) {
+		String[] splitString = stringInput.split(" ");
 
-    public static Parser getInstance () { 
-    	if ( instance == null ) {
-    		instance = new Parser();
-    	}
-    	return instance;
-    }
-    
-    public boolean parseInput(Task myTask) {
-    	
-    	// find out what kind of task, is it valid
-    	// call storage for operations
-    	Storage.getInstance().writeToFile(); // or equivalent
-    	return true;
-    }
+		return splitString[0].toLowerCase(); 
+	}
+	
+	
+	/**
+	 * FOR FLOATING TASK: 
+	 * Given a stringInput, remove the command from the string
+	 * @param command
+	 * @param stringInput
+	 * @return taskName without command
+	 */
+	public static String getTaskName(String command, String stringInput) {
+		String task = stringInput.replaceFirst(command, "");
+		
+		return task.trim(); 
+	}
+	
+	
+	/**
+	 * FOR TASK WITH DATES:
+	 * Given a stringInput, remove the command and date from the string
+	 * @param command
+	 * @param date
+	 * @param stringInput
+	 * @return taskName without command and date 
+	 */
+	public static String getTaskName(String command, String date, String stringInput) {
+		String task = stringInput.replaceFirst(command, "");
+		task = task.replaceFirst(date, ""); 
+		
+		return task.trim(); 
+	}
+	
+	//TODO: figure out when to remove keywords from commands 
+	private String joinStringWithoutKeywords(ArrayList<String> dates) {
+		 
+		return ""; 
+	}
+	
+	//TODO: Dummy for now  
+	public ArrayList<String> getDate(String stringInput) {
+			
+		return new ArrayList<String>(); 
+	}
+	
+	
+	public static void main(String[] args) {
+		String myString = getTaskName("add","add taskey to github");
+		System.out.println(myString);
+	}
 }

0ab6a2d02a89edc9c69ac710e836a4f1d0f0eaba
Hubert <Lvl100p@gmail.com>
2016-02-14 18:02:47 +0800

    Refactored Logic.java to use Parser class methods

--- taskey/src/taskey/logic/Logic.java
+++ taskey/src/taskey/logic/Logic.java
@@ -2,84 +2,112 @@ package taskey.logic;
 
 import taskey.parser.Parser;
 import taskey.storage.Storage;
+import taskey.logic.Task;
+import taskey.logic.ProcessedObject;
 
-enum StatusCode {
-	COMMAND_INVALID, COMMAND_SUCCESS, ADD_FLOATING, ADD_DEADLINED, ADD_EVENT, ADD_RECURRING, DELETE, UPDATE,
-	UNDO, VIEW
-}
-
+/**
+ * //TODO: class description
+ * 
+ * @author Hubert Wong
+ */
 public class Logic {
+	private Parser parser;
+	private Storage storage;
+	/** The most recent task whose command is not VIEW or UNDO */
+	private Task mostRecentTask = null;
+	
+	//TODO: constructors
+	public Logic() {
+		parser = new Parser();
+		storage = new Storage();
+	}
 
-	private static Logic instance = null;
-	private static Task mostRecent = null;
-
-    public static Logic getInstance() { 
-    	if ( instance == null ) {
-    		instance = new Logic();
-    	}
-    	return instance;
-    }
-    
-    /*
-     * Attemps to execute a command specified by line. 
+    /**
+     * Attempts to execute a command specified by the input string.
+     * 
+     * @param input the input string
+     * @return      status code reflecting the outcome of command execution
      */
-    public int executeCommand(String line) {
-    	/*Parser parser = new Parser(); 
-    	Storage storage = new Storage();
-    	Task task = parser.parseInput(line); 
+    public int executeCommand(String input) {
+    	ProcessedObject po = parser.parseInput(input);
+    	String command = po.getCommand();
+    	Task task = po.getTask();
     	
-    	if (task == null) { //Command is invalid
-    		return COMMAND_INVALID;
-    	}
+    	//TODO: handle invalid commands
     	
-    	else {
-    		StatusCode status = task.getStatusCode();
-    		String taskName = task.getName();
+    	String taskName = task.getName();	
+    	switch (command) {
+    		case VIEW:
+    			//TODO: send view request to storage
+    			break;
+    			
+    		case ADD_FLOATING:
+    			//TODO: add floating task to storage
+    			break;
+    			
+    		case ADD_DEADLINE:
+    			long deadlineEpoch = task.getDeadlineEpoch();
+    			//TODO: add deadlined task to storage
+    			break;
+    				
+    		case ADD_EVENT:
+    			long startDateEpoch = task.getStartDateEpoch();
+    			long endDateEpoch = task.getEndDateEpoch();
+    			//TODO: add event task to storage
+    			break;
+    				
+    		case ADD_RECURRING:
+    			//TODO: add recurring task to storage
+    			break;
+    			
+    		case DELETE_BY_INDEX:
+    			//TODO: delete indexed task from storage
+    			break;
     		
-    		switch (status) {
-    			case ADD_FLOATING:
-    				//addTask(name, deadline, start date, end date, recurring date)
-    				storage.addTask(taskName, null, null, null, null);
-    				break;
-    			
-    			case ADD_DEADLINED:
-    				Date deadline = task.getDeadline();
-    				storage.addTask(taskName, deadline, null, null, null);
-    				break;
+    		case DELETE_BY_NAME:
+    			//TODO: delete named task from storage
+    			break;
+    		
+    		case UPDATE_BY_INDEX:
+    			//TODO: update indexed task in storage
+    			break;
+    		
+    		case UPDATE_BY_NAME:
+    			//TODO: update named task in storage
+    			break;
+    		
+    		case UNDO:
+    			String mostRecentCommand = mostRecentTask.getCommand();
+    			switch (mostRecentCommand) {
+    				case ADD_FLOATING:
+    				case ADD_DEADLINE:
+    				case ADD_EVENT:
+    				case ADD_RECURRING:
+    					//TODO: delete most recently added task from storage
+    					break;
     				
-    			case ADD_EVENT:
-    				Date startDate = task.getStartDate();
-    				Date endDate = task.getEndDate();
-    				storage.addTask(taskName, null, startDate, endDate, null);
-    				break;
+    				case DELETE_BY_INDEX:
+    				case DELETE_BY_NAME:
+    					//TODO: add most recently deleted task to storage
+    					break;
     				
-    			case ADD_RECURRING:
-    				Date recurringDate = task.getRecurringDate();
-    				storage.addTask(taskName, null, null, null, recurringDate);
-    				break;
+    				case UPDATE_BY_INDEX:
+    				case UPDATE_BY_NAME:
+    					//TODO: revert most recently updated task in storage
+    					break;
+    				
+    				default:
+    			}
+    			break;
+    		
+    		case ERROR:
+    			//TODO
     			
-    			case DELETE:
-    				storage.deleteTask(taskName);
-    				break;
-    			
-    			case UPDATE:
-    				???
-    			
-    			case UNDO:
-    				StatusCode mostRecentStatus = mostRecent.getStatusCode();
-    				if (mostRecentStatus == ADD_FLOATING || mostRecentStatus == ADD_DEADLINED
-    					|| mostRecentStatus == ADD_EVENT || mostRecentStatus == ADD_RECURRING) {
-    					storage.deleteTask(mostRecent.getName());
-    				} else if (mostRecentStatus == DELETE) {
-    					???
-    				}
-    				break;
-    			
-    			default:
-    		}
-    	}*/
-    	return 0;
+    		default:
+    	}
     	// Update UI 
     	// UiController.getInstance().update();
+    	
+    	return -1; //stub
     }
 }

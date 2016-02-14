ddcbd2bcedb1b23afc58a35c59ae321875b43771
XueHui <nattaro13@gmail.com>
2016-02-14 14:38:36 +0800

    Added non-basic methods and filled up comparator

--- taskey/src/taskey/logic/Task.java
+++ taskey/src/taskey/logic/Task.java
@@ -2,23 +2,31 @@ package taskey.logic;
 
 import taskey.parser.TimeConverter; 
 
+/**
+ * Task object holds all the details of the task 
+ * @author Xue Hui 
+ *
+ */
+
 public class Task implements Comparable<Task>{
 	public static final int NONE = -1; 
+	public static final String EMPTY = ""; 
 	
 	private String taskName;
 	private String taskDetails;
 	private String taskType; //floating,event (has start and end) , work (has a deadline)
 	private long[] datesEpoch = {NONE,NONE,NONE,NONE}; 
-	private String[] datesHuman = {"","","",""};
+	private String[] datesHuman = {EMPTY,EMPTY,EMPTY,EMPTY};
 	private boolean isRecurring = false; //set to true if it is recurring. 
-	//dates*[0]: recurring date (if recurring event) 
+	//dates*[0]: recurring interval (if recurring event) 
 	//dates*[1]: start Time (events)
 	//date*[2]: end Time (events) 
 	//date*[3]: deadline (work) 
 	//TODO: Date Array TBC 
-	
+	//idea for recurring : store the diff in time to the next recurrence in seconds?
 	private TimeConverter timeConverter = new TimeConverter(); 
 	
+	//CONSTRUCTORS ==============================================
 	public Task() {
 		taskName = "";
 		taskDetails = "";
@@ -37,6 +45,8 @@ public class Task implements Comparable<Task>{
 		taskType = ""; 
 	}
 	
+	
+	//BASIC GET/SET METHODS =====================================
 	public String getTaskName() {
 		return taskName;
 	}
@@ -69,12 +79,20 @@ public class Task implements Comparable<Task>{
 		return 1; 
 	}
 	
-	public void setStartDate() {
+	public void setStartDate(String startDate) {
 		//TODO 
 	}
 	
-	public void setEndDate() {
+	public void setStartDate(long startDate) {
+		
+	}
+	
+	public void setEndDate(String endDate) {
 		//TODO 
+	}
+	
+	public void setEndDate(long endDate) {
+		
 	}
 	
 	public boolean getIsRecurring() {
@@ -85,17 +103,66 @@ public class Task implements Comparable<Task>{
 		this.isRecurring = isRecurring; 
 	}
 	
-	@Override
-	//tasks are comparable by their time. 
-	public int compareTo(Task anotherTask) {
-		// TODO Auto-generated method stub
-		return 0;
+	
+	//NON-BASIC METHODS ==========================================
+	
+	public String getDeadline() {
+		return datesHuman[3]; 
 	}
 	
-	//tasks are the same if they have the same name
-	//used for update and delete. 
+	public long getDeadlineEpoch() {
+		return datesEpoch[3]; 
+	}
+	
+	public String[] getEventTime() {
+		String[] eventTime = {datesHuman[1],datesHuman[2]};
+		return eventTime; 
+	}
+	
+	public long[] getEventTimeEpoch() {
+		long[] eventTime = {datesEpoch[1],datesEpoch[2]}; 
+		return eventTime; 
+	}
+	
+	public String getRecurringTime() {
+		return datesHuman[0]; 
+	}
+	
+	public long getRecurringTimeEpoch() {
+		return datesEpoch[0]; 
+	}
+	
+	
+	@Override
+	/**
+	 * tasks are comparable by their time. 
+	 * used for sorting in ArrayList<Task>
+	 * So that one can easily just call Collections.sort(taskList) 
+	 */
+	public int compareTo(Task anotherTask) {
+		long startTime = datesEpoch[1]; 
+		long otherStartTime = anotherTask.getStartDate(); 
+		
+		if (startTime > otherStartTime) {
+			return 1; 
+		} else if (startTime == otherStartTime) {
+			return 0;
+		} else {
+			return -1;
+		} 
+	}
+	
+	/**
+	 * tasks are the same if they have the same name
+	 * used for UPDATE_BY_NAME and DELETE_BY_NAME
+	 * @param anotherTask
+	 * @return 0 if they have the same name, else return -1 
+	 */
 	public int equals(Task anotherTask) {
-		return 0; 
+		if (taskName.compareTo(anotherTask.getTaskName()) == 0) {
+			return 0; 
+		}	
+		return -1; 
 	}
 
 }

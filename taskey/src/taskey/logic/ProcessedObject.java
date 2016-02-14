99f98eb3bcbc4673d727a1d18f0cb039afae96d4
XueHui <nattaro13@gmail.com>
2016-02-14 13:00:14 +0800

    Added class description and deleteIndex attribute

--- taskey/src/taskey/logic/ProcessedObject.java
+++ taskey/src/taskey/logic/ProcessedObject.java
@@ -1,12 +1,35 @@
 package taskey.logic;
 
+/**
+ * This class will be used for facilitating transfer of a processed task from 
+ * Parser to Logic. 
+ * 
+ * command types:
+ * 1. ADD_FLOATING
+ * 2. ADD_DEADLINE
+ * 3. ADD_EVENT
+ * 4. ADD_RECURRING
+ * 5. DELETE
+ * 6. UPDATE
+ * 7. VIEW 
+ * 
+ * @author Xue Hui
+ *
+ */
 public class ProcessedObject {
 	private String command;
 	private Task task; 
+	private int deleteIndex = -1; //only used if command is delete 
 	
 	public ProcessedObject(String command, Task task) {
 		this.command = command;
 		this.task = task; 
+	}
+	
+	public ProcessedObject(String command, Task task, int deleteIndex) {
+		this.command = command;
+		this.task = task; 
+		this.deleteIndex = deleteIndex; 
 	}
 	
 	public String getCommand() {
@@ -23,6 +46,14 @@ public class ProcessedObject {
 	
 	public void setTask(Task task) {
 		this.task = task; 
+	}
+	
+	public int getDeleteIndex() {
+		return deleteIndex; 
+	}
+	
+	public void setDeleteIndex(int deleteIndex) {
+		this.deleteIndex = deleteIndex; 
 	}
 
 }

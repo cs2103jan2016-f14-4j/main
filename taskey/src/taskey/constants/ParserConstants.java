28528aabdf0875f834fcf687115f29fa75a7f135
XueHui <nattaro13@gmail.com>
2016-02-29 17:12:45 +0800

    Constants: added ParserConstants.java

---
+++ taskey/src/taskey/constants/ParserConstants.java
@@ -0,0 +1,16 @@
+package taskey.constants;
+
+public class ParserConstants {
+	public static final String DELETE_BY_INDEX = "DELETE_BY_INDEX"; 
+	public static final String DELETE_BY_NAME = "DELETE_BY_NAME"; 
+	public static final String DONE_BY_INDEX = "DONE_BY_INDEX"; 
+	public static final String DONE_BY_NAME = "DONE_BY_NAME"; 
+	public static final String UPDATE_BY_INDEX_CHANGE_NAME = "UPDATE_BY_INDEX_CHANGE_NAME"; 
+	public static final String UPDATE_BY_INDEX_CHANGE_DATE = "UPDATE_BY_INDEX_CHANGE_DATE"; 
+	public static final String UPDATE_BY_NAME_CHANGE_NAME = "UPDATE_BY_NAME_CHANGE_NAME";
+	public static final String UPDATE_BY_NAME_CHANGE_DATE = "UPDATE_BY_NAME_CHANGE_DATE";
+	public static final String DAY_END = "23:59:59"; 
+	public static final String ERROR_DATE_FORMAT = "Wrong date format"; 
+	public static final String ERROR_VIEW_TYPE = "No such category"; 
+	public static final String ERROR_COMMAND = "No such command"; 
+}

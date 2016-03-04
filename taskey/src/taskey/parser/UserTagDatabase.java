a5231938e0273ba2b878d4268af0f6cec35dc555
XueHui <nattaro13@gmail.com>
2016-03-04 17:20:08 +0800

    UserTagDB: added load() skeleton

--- taskey/src/taskey/parser/UserTagDatabase.java
+++ taskey/src/taskey/parser/UserTagDatabase.java
@@ -10,6 +10,10 @@ public class UserTagDatabase {
 		//initialise the database of tags. 
 	}
 	
+	public void loadDatabase() {
+		//load the file. 
+	}
+	
 	/**
 	 * Add a new tag to the userTagDatabase
 	 * @param tag

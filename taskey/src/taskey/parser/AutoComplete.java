d74ee3ed29b4ea2f024f43face00c0cf6c9a0f7b
XueHui <nattaro13@gmail.com>
2016-03-08 23:51:07 +0800

    AutoComplete.java: added skeleton for completeView()

--- taskey/src/taskey/parser/AutoComplete.java
+++ taskey/src/taskey/parser/AutoComplete.java
@@ -11,6 +11,7 @@ import java.util.ArrayList;
  */
 public class AutoComplete {
 	private ArrayList<String> commands = new ArrayList<String>();
+	private ArrayList<String> viewList = new ArrayList<String>();
 	
 	public AutoComplete() {
 		commands.add("add");
@@ -21,13 +22,21 @@ public class AutoComplete {
 		commands.add("done");
 		commands.add("undo");
 		commands.add("file_loc");
+		
+		viewList.add("all");
+		viewList.add("general");
+		viewList.add("deadlines");
+		viewList.add("events");
+		viewList.add("archive");
+		viewList.add("help");
+		
 	}
 	
 	/**
 	 * Given a partial input of the command, return a list of commands 
 	 * that contain that input as a sub-string. 
 	 * @param phrase
-	 * @return
+	 * @return If no such command exists, return null
 	 */
 	public ArrayList<String> completeCommand(String phrase) {
 		phrase = phrase.toLowerCase(); 
@@ -41,10 +50,32 @@ public class AutoComplete {
 		
 		if (!availCommands.isEmpty()) {
 			return availCommands;
-		} else {
-			//no such command containing that sub-string
-			return null; 
-		}
+		} 
+		//no such command containing that sub-string
+		return null; 
 	}
-
+	
+	/**
+	 * Given a partial input that contains "view xxxx",
+	 * return a list of available view types that the user can access.
+	 * @param phrase
+	 * @return If no such list of views is available, return null 
+	 */
+	public ArrayList<String> completeView(String phrase) {
+		ArrayList<String> availViews = new ArrayList<String>();
+		phrase = phrase.toLowerCase();
+		phrase = phrase.replaceFirst("view", ""); 
+		
+		for(int i = 0; i < viewList.size(); i++) {
+			if (viewList.get(i).contains(phrase)) {
+				availViews.add(viewList.get(i)); 
+			}
+		}
+		//TODO: Handle case for tagged categories
+		
+		if (!availViews.isEmpty()) {
+			return availViews; 
+		}
+		return null; 
+	}
 }

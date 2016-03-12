8e92d214c8cfcdac526c4f1d0cb4e2efe1cba160
Hubert <Lvl100p@gmail.com>
2016-03-12 14:10:14 +0800

    Wrote unit test for case ADD_FLOATING

--- taskey/src/taskey/logic/Logic.java
+++ taskey/src/taskey/logic/Logic.java
@@ -226,8 +226,8 @@ public class Logic {
                 				 new Exception("Failed to execute command.")); //Stub
 	}
 
-	//Adds a floating task to the relevant lists.
-	private LogicFeedback addFloating(Task task, ProcessedObject po) {
+	//Adds a floating task to the relevant lists, and saves the updated lists to disk.
+	protected LogicFeedback addFloating(Task task, ProcessedObject po) {
 		ArrayList<Task> pendingList = taskLists.get(ListID.PENDING.getIndex());
 
 		if (pendingList.contains(task)) { //Duplicate task names not allowed
--- taskey/src/taskey/logic/LogicTest.java
+++ taskey/src/taskey/logic/LogicTest.java
@@ -5,14 +5,16 @@ import java.util.ArrayList;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 
+import taskey.logic.LogicConstants.ListID;
 import taskey.ui.UiConstants.ContentBox;
 
 public class LogicTest {
 	
-	@Test
-	public void testClear() {
+	@BeforeClass
+	public static void testClear() {
 		Logic logic = new Logic();
 		LogicFeedback actual = logic.executeCommand(ContentBox.PENDING, "clear");
 		ArrayList<ArrayList<Task>> temp = new ArrayList<ArrayList<Task>>();
@@ -24,10 +26,22 @@ public class LogicTest {
 		assertTrue(actual.equals(expected));
 	}
 	
-	/*@Test
+	@Test
 	public void testAddFloating() {
 		Logic logic = new Logic();
-		Task t = new Task("oneword");
+		logic.executeCommand(ContentBox.PENDING, "clear");
+		Task t = new Task("g2 a?b ,  ");
 		ProcessedObject po = new ProcessedObject("FLOATING", t);
-	}*/
+		LogicFeedback actual = logic.addFloating(t, po);
+		
+		ArrayList<ArrayList<Task>> temp = new ArrayList<ArrayList<Task>>();
+		while (temp.size() < 7) {
+			temp.add(new ArrayList<Task>());
+		}
+		temp.get(ListID.PENDING.getIndex()).add(new Task("g2 a?b ,  "));
+		temp.get(ListID.GENERAL.getIndex()).add(new Task("g2 a?b ,  "));
+		LogicFeedback expected = new LogicFeedback(temp, po, null);
+		
+		assertTrue(actual.equals(expected));
+	}
 }

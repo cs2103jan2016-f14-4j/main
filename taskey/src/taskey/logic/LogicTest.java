0c61399e1d42773ead16ba74d5518b2a40439fb9
Hubert <Lvl100p@gmail.com>
2016-03-12 14:44:09 +0800

    Added unit tests for ADD_DEADLINE

--- taskey/src/taskey/logic/LogicTest.java
+++ taskey/src/taskey/logic/LogicTest.java
@@ -9,6 +9,8 @@ import org.junit.Ignore;
 import org.junit.Test;
 
 import taskey.logic.LogicConstants.ListID;
+import taskey.parser.Parser;
+import taskey.parser.TimeConverter;
 import taskey.ui.UiConstants.ContentBox;
 
 public class LogicTest {
@@ -42,6 +44,50 @@ public class LogicTest {
 		}
 		temp.get(ListID.PENDING.getIndex()).add(t);
 		temp.get(ListID.GENERAL.getIndex()).add(t);
+		LogicFeedback expected = new LogicFeedback(temp, po, null);
+		
+		assertTrue(actual.equals(expected));
+	}
+	
+	@Test
+	public void testAddDeadlineForThisWeek() {
+		Logic logic = new Logic();
+		Parser parser = new Parser();
+		TimeConverter timeConverter = new TimeConverter();
+		logic.executeCommand(ContentBox.PENDING, "clear");
+		String input = "add g2 a?b ,  on " + timeConverter.getDate(timeConverter.getCurrTime());
+		ProcessedObject po = parser.parseInput(input);
+		Task t = po.getTask();
+		LogicFeedback actual = logic.addDeadline(t, po);
+		
+		ArrayList<ArrayList<Task>> temp = new ArrayList<ArrayList<Task>>();
+		while (temp.size() < 7) {
+			temp.add(new ArrayList<Task>());
+		}
+		temp.get(ListID.PENDING.getIndex()).add(t);
+		temp.get(ListID.DEADLINE.getIndex()).add(t);
+		temp.get(ListID.THIS_WEEK.getIndex()).add(t);
+		LogicFeedback expected = new LogicFeedback(temp, po, null);
+		
+		assertTrue(actual.equals(expected));
+	}
+	
+	@Test
+	public void testAddDeadlineOutsideThisWeek() {
+		Logic logic = new Logic();
+		Parser parser = new Parser();
+		logic.executeCommand(ContentBox.PENDING, "clear");
+		String input = "add g2 a?b ,  on 12 mar 2017"; //Stub for now
+		ProcessedObject po = parser.parseInput(input);
+		Task t = po.getTask();
+		LogicFeedback actual = logic.addDeadline(t, po);
+		
+		ArrayList<ArrayList<Task>> temp = new ArrayList<ArrayList<Task>>();
+		while (temp.size() < 7) {
+			temp.add(new ArrayList<Task>());
+		}
+		temp.get(ListID.PENDING.getIndex()).add(t);
+		temp.get(ListID.DEADLINE.getIndex()).add(t);
 		LogicFeedback expected = new LogicFeedback(temp, po, null);
 		
 		assertTrue(actual.equals(expected));

2dda02fe208bfb28e0e901919cf257cf06a205dc
dq-dx <clor3x@gmail.com>
2016-03-27 07:55:01 +0800

    indent

--- taskey/src/taskey/storage/StorageReader.java
+++ taskey/src/taskey/storage/StorageReader.java
@@ -18,55 +18,55 @@ class StorageReader {
 	@SuppressWarnings("serial")
 	public class InvalidTaskException extends Exception {
 	}
-	
-    /**
-     * Generic read method. Deserializes the JSON specified by src into an object of the specified type.
-     * @param src JSON file to be read
-     * @param typeToken represents the generic type T of the desired object; this is obtained from the Gson TypeToken class
-     * @return An object of type T generated from the JSON file.
-     * @throws FileNotFoundException if FileReader constructor fails
-     */
-    private <T> T readFromFile(File src, TypeToken<T> typeToken) throws FileNotFoundException {
-    	FileReader reader = new FileReader(src);
-    	Gson gson = new Gson();
+
+	/**
+	 * Generic read method. Deserializes the JSON specified by src into an object of the specified type.
+	 * @param src JSON file to be read
+	 * @param typeToken represents the generic type T of the desired object; this is obtained from the Gson TypeToken class
+	 * @return An object of type T generated from the JSON file.
+	 * @throws FileNotFoundException if FileReader constructor fails
+	 */
+	private <T> T readFromFile(File src, TypeToken<T> typeToken) throws FileNotFoundException {
+		FileReader reader = new FileReader(src);
+		Gson gson = new Gson();
 		T object = gson.fromJson(reader, typeToken.getType()); //TODO Handle type safety
 		return object;
-    }
+	}
 
-    /*================*
-     * Load directory *
-     *================*/
-    /**
-     * Tries to read the last-saved directory from a config file located in System.getProperty("user.dir").
-     * @param filename name of the config file to be read
-     * @return the File representing the last-saved directory, or null if it was not found
-     */
-    File loadDirectory(String filename) {
-    	File src = new File(filename);
-    	try {
-    		//TODO: buggyPath will somehow always have user.dir prefixed in its absolute path
-    		File buggyPath = readFromFile(src, new TypeToken<File>() {});
-    		File fixedPath = new File(buggyPath.getPath()); //hackey solution
-    		return fixedPath;
-    	} catch (FileNotFoundException e) {
-    		return null;
-    	}
-    }
-    
+	/*================*
+	 * Load directory *
+	 *================*/
+	/**
+	 * Tries to read the last-saved directory from a config file located in System.getProperty("user.dir").
+	 * @param filename name of the config file to be read
+	 * @return the File representing the last-saved directory, or null if it was not found
+	 */
+	File loadDirectory(String filename) {
+		File src = new File(filename);
+		try {
+			//TODO: buggyPath will somehow always have user.dir prefixed in its absolute path
+			File buggyPath = readFromFile(src, new TypeToken<File>() {});
+			File fixedPath = new File(buggyPath.getPath()); //hackey solution
+			return fixedPath;
+		} catch (FileNotFoundException e) {
+			return null;
+		}
+	}
 
-    /*================*
-     * Load task list *
-     *================*/
-    /**
-     * Returns an ArrayList of Task objects read from the File src.
-     * An empty ArrayList is returned if src was not found.
-     * @param src the source file to be read from
-     * @return the tasklist read from file; or an empty list if the file was not found
-     * @throws FileNotFoundException 
-     * @throws InvalidTaskException 
-     */
-    ArrayList<Task> loadTasklist(File src) throws FileNotFoundException, InvalidTaskException {
-    	ArrayList<Task> tasklist;
+
+	/*================*
+	 * Load task list *
+	 *================*/
+	/**
+	 * Returns an ArrayList of Task objects read from the File src.
+	 * An empty ArrayList is returned if src was not found.
+	 * @param src the source file to be read from
+	 * @return the tasklist read from file; or an empty list if the file was not found
+	 * @throws FileNotFoundException 
+	 * @throws InvalidTaskException 
+	 */
+	ArrayList<Task> loadTasklist(File src) throws FileNotFoundException, InvalidTaskException {
+		ArrayList<Task> tasklist;
 		try {
 			tasklist = readFromFile(src, new TypeToken<ArrayList<Task>>() {});
 			verifyTasklist(tasklist);
@@ -77,38 +77,38 @@ class StorageReader {
 			System.out.println("{Tasklist invalid} " + src.getName());
 			throw e;
 		}
-    	return tasklist;
-    }
-    
-    /**
-     * Sanity check for when reading tasklists.
-     * @param tasklist
-     * @throws StorageException
-     */
-    void verifyTasklist(ArrayList<Task> tasklist) throws InvalidTaskException {
-    	for (Task t : tasklist) {
-    		if (t.getTaskType() == null) {
-    			throw new InvalidTaskException();
-    		}
-    	}
-    }
-    
-    /*===========*
-     * Load tags *
-     *===========*/
-    /**
-     * Returns an ArrayList of Tags read from the File src.
-     * An empty ArrayList is returned if src was not found.
-     * @param src source file to be read
-     * @return ArrayList containing the user-defined tags, or an empty ArrayList if src was not found
-     */
-    ArrayList<TagCategory> loadTaglist(File src) {
-    	ArrayList<TagCategory> tags;
-    	try {
-    		tags = readFromFile(src, new TypeToken<ArrayList<TagCategory>>() {});
-    	} catch (FileNotFoundException e) {
-    		tags = new ArrayList<TagCategory>();
-    	}
-    	return tags;
-    }
+		return tasklist;
+	}
+
+	/**
+	 * Sanity check for when reading tasklists.
+	 * @param tasklist
+	 * @throws StorageException
+	 */
+	void verifyTasklist(ArrayList<Task> tasklist) throws InvalidTaskException {
+		for (Task t : tasklist) {
+			if (t.getTaskType() == null) {
+				throw new InvalidTaskException();
+			}
+		}
+	}
+
+	/*===========*
+	 * Load tags *
+	 *===========*/
+	/**
+	 * Returns an ArrayList of Tags read from the File src.
+	 * An empty ArrayList is returned if src was not found.
+	 * @param src source file to be read
+	 * @return ArrayList containing the user-defined tags, or an empty ArrayList if src was not found
+	 */
+	ArrayList<TagCategory> loadTaglist(File src) {
+		ArrayList<TagCategory> tags;
+		try {
+			tags = readFromFile(src, new TypeToken<ArrayList<TagCategory>>() {});
+		} catch (FileNotFoundException e) {
+			tags = new ArrayList<TagCategory>();
+		}
+		return tags;
+	}
 }
--- taskey/src/taskey/storage/StorageWriter.java
+++ taskey/src/taskey/storage/StorageWriter.java
@@ -16,75 +16,75 @@ import taskey.logic.Task;
  * @@author A0121618M
  */
 class StorageWriter {
-    /**
-     * Generic write method. Serializes the given object of the specified type into its equivalent JSON representation.
-     * @param dest the JSON file to be written
-     * @param object of type T to be serialized
-     * @param typeToken represents the generic type T of the given object; this is obtained from the Gson TypeToken class
-     * @throws IOException
-     */
-    private <T> void writeToFile(File dest, T object, TypeToken<T> typeToken) throws IOException {
-    	FileWriter writer = new FileWriter(dest);
-    	Gson gson = new GsonBuilder().setPrettyPrinting().create();
-    	String jsonOutput = gson.toJson(object, typeToken.getType());
-    	writer.write(jsonOutput);
-    	writer.close();
-    }
+	/**
+	 * Generic write method. Serializes the given object of the specified type into its equivalent JSON representation.
+	 * @param dest the JSON file to be written
+	 * @param object of type T to be serialized
+	 * @param typeToken represents the generic type T of the given object; this is obtained from the Gson TypeToken class
+	 * @throws IOException
+	 */
+	private <T> void writeToFile(File dest, T object, TypeToken<T> typeToken) throws IOException {
+		FileWriter writer = new FileWriter(dest);
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		String jsonOutput = gson.toJson(object, typeToken.getType());
+		writer.write(jsonOutput);
+		writer.close();
+	}
 
-    /*================*
-     * Save directory *
-     *================*/
-    /**
-     * Writes the given directory to a config file located in System.getProperty("user.dir").
-     * @param dir the File representing the directory to be saved
-     * @param filename name of the destination file
-     */
-    void saveDirectory(File dir, String filename) {
-    	File dest = new File(filename);
-    	try {
-    		writeToFile(dest, dir.getCanonicalFile(), new TypeToken<File>() {});
-    		//System.out.println("{New storage directory saved}");
-    	} catch (IOException e) {
-    		e.printStackTrace();
-    		System.out.println("{Error saving new directory}");
-    	}
-    }
+	/*================*
+	 * Save directory *
+	 *================*/
+	/**
+	 * Writes the given directory to a config file located in System.getProperty("user.dir").
+	 * @param dir the File representing the directory to be saved
+	 * @param filename name of the destination file
+	 */
+	void saveDirectory(File dir, String filename) {
+		File dest = new File(filename);
+		try {
+			writeToFile(dest, dir.getCanonicalFile(), new TypeToken<File>() {});
+			//System.out.println("{New storage directory saved}");
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.out.println("{Error saving new directory}");
+		}
+	}
 
-    /*================*
-     * Save task list *
-     *================*/
-    /**
-     * Writes an ArrayList of Task objects to the File dest.
-     * The file will be created if it doesn't exist; otherwise the existing file will be overwritten.
-     * @param tasks the tasklist to be saved
-     * @param dest the destination file to be written to
-     * @throws IOException when there is error writing to file
-     */
-    void saveTasklist(ArrayList<Task> tasks, File dest) throws IOException {
+	/*================*
+	 * Save task list *
+	 *================*/
+	/**
+	 * Writes an ArrayList of Task objects to the File dest.
+	 * The file will be created if it doesn't exist; otherwise the existing file will be overwritten.
+	 * @param tasks the tasklist to be saved
+	 * @param dest the destination file to be written to
+	 * @throws IOException when there is error writing to file
+	 */
+	void saveTasklist(ArrayList<Task> tasks, File dest) throws IOException {
 		try {
 			writeToFile(dest, tasks, new TypeToken<ArrayList<Task>>() {});
 		} catch (IOException e) {
 			e.printStackTrace();
 			throw e;
 		}
-    }
-    
-    /*========================*
-     * Save tags - New method *
-     *========================*/
-    /**
-     * Saves the given ArrayList of Tags to the File dest.
-     * The file will be created if it doesn't exist; otherwise the existing file will be overwritten.
-     * @param tags ArrayList containing the user-defined tags
-     * @param dest the destination file to be written to
-     * @throws IOException when there is error writing to file
-     */
-    void saveTaglist(ArrayList<TagCategory> tags, File dest) throws IOException {
-    	try {
-    		writeToFile(dest, tags, new TypeToken<ArrayList<TagCategory>>() {});
-    	} catch (IOException e) {
-    		e.printStackTrace();
-    		throw e;
-    	}
-    }
+	}
+
+	/*===========*
+	 * Save tags *
+	 *===========*/
+	/**
+	 * Saves the given ArrayList of Tags to the File dest.
+	 * The file will be created if it doesn't exist; otherwise the existing file will be overwritten.
+	 * @param tags ArrayList containing the user-defined tags
+	 * @param dest the destination file to be written to
+	 * @throws IOException when there is error writing to file
+	 */
+	void saveTaglist(ArrayList<TagCategory> tags, File dest) throws IOException {
+		try {
+			writeToFile(dest, tags, new TypeToken<ArrayList<TagCategory>>() {});
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw e;
+		}
+	}
 }

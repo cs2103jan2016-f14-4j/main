ed65edd8429061f403e685ca5229672fec2c6760
Hubert <Lvl100p@gmail.com>
2016-03-28 05:13:16 +0800

    Fixed import

--- taskey/test/taskey/junit/LogicTest.java
+++ taskey/test/taskey/junit/LogicTest.java
@@ -4,11 +4,11 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.ocpsoft.prettytime.shade.edu.emory.mathcs.backport.java.util.Collections;
 
 import taskey.constants.UiConstants.ContentBox;
 import taskey.logic.Logic;

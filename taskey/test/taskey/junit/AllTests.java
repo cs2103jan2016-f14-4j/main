fac5879e8cbef1a43523329d7a468ddbc822b9dd
tgogreat <tgogreat@hotmail.com>
2016-04-02 21:25:35 +0800

    Added Test Suite, Log Test

--- taskey/test/taskey/junit/AllTests.java
+++ taskey/test/taskey/junit/AllTests.java
@@ -5,7 +5,13 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({})
+@SuiteClasses({
+	 LogicTest.class, 
+	 ParserTest.class,
+	 StorageTest.class,
+	 TimeConverterTest.class,
+	 LoggerTest.class,
+	 IntegrationTest.class
+})
 public class AllTests {
-
 }
